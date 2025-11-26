e0f1da3bbbc236277c9d5d769139f17060e80ede
macanmaxon <chekushin.maxim031007@yandex.ru>
2025-11-26 18:29:27 +0300

    ff

---
+++ task_3/task_3_1/task_3_1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void printStudentInfo() {
+    cout << "Student: John Doe" << endl;
+    cout << "Group: CS-21" << endl;
+    cout << "Task: 3.1" << endl;
+    cout << "------------------------" << endl;
+}
+
+void createBooksFile() {
+    ofstream file("books.txt");
+    if (!file.is_open()) {
+        cout << "Error creating books.txt file!" << endl;
+        return;
+    }
+    
+    file << "1. Name: To Kill a Mockingbird" << endl;
+    file << "   Author: Harper Lee" << endl;
+    file << "   Year: 1960" << endl;
+    file << "   ----------" << endl;
+    
+    file << "2. Name: 1984" << endl;
+    file << "   Author: George Orwell" << endl;
+    file << "   Year: 1949" << endl;
+    file << "   ----------" << endl;
+    
+    file << "3. Name: Pride and Prejudice" << endl;
+    file << "   Author: Jane Austen" << endl;
+    file << "   Year: 1813" << endl;
+    file << "   ----------" << endl;
+    
+    file << "4. Name: The Great Gatsby" << endl;
+    file << "   Author: F. Scott Fitzgerald" << endl;
+    file << "   Year: 1925" << endl;
+    file << "   ----------" << endl;
+    
+    file << "5. Name: Moby Dick" << endl;
+    file << "   Author: Herman Melville" << endl;
+    file << "   Year: 1851" << endl;
+    file << "   ----------" << endl;
+    
+    file.close();
+    cout << "File books.txt created successfully with 5 book entries!" << endl;
+}
+
+int main() {
+    printStudentInfo();
+    createBooksFile();
+    return 0;
+}
